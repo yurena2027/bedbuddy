719f45f4b8830c883fcfad2b87ede00b8ba4d1f7
tkaes <tkuga@uccs.edu>
2025-10-10 20:47:59 -0600

    Create basic db_config.py

--- config/db_config.py
+++ config/db_config.py
@@ -1,1 +1,10 @@
 # Setup db information
+from pymongo import MongoClient
+
+# MongoDB Atlas connection string
+MONGO_URI = "YOUR MONGO URI"
+DB_NAME = "bedbuddy"
+
+def get_db():
+    client = MongoClient(MONGO_URI)
+    return client[DB_NAME]
